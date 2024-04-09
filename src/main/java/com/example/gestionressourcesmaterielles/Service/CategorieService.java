87f102761bd8686f0b73ac3b6c00fe83041c7f93
Jawher-0129 <jawhertalbi12@gmail.com>
2024-04-09 15:39:35 +0100

    Ajout Materiel

---
+++ src/main/java/com/example/gestionressourcesmaterielles/Service/CategorieService.java
@@ -0,0 +1,115 @@
+package com.example.gestionressourcesmaterielles.Service;
+
+import com.example.gestionressourcesmaterielles.Model.Categorie;
+import com.example.gestionressourcesmaterielles.Model.IService;
+import com.example.gestionressourcesmaterielles.util.DataSource;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategorieService implements IService<Categorie> {
+    private Connection cnx = DataSource.getInstance().getConnetion();
+    private Statement ste;
+    private PreparedStatement pst;
+
+    public CategorieService() {
+    }
+
+    public void add(Categorie c) {
+        String requete = "insert into categorie(libelle_categorie) values('" + c.getLibelle() + "')";
+
+        try {
+            this.ste = this.cnx.createStatement();
+            this.ste.executeUpdate(requete);
+        } catch (SQLException var4) {
+            throw new RuntimeException(var4);
+        }
+    }
+
+    public void delete(int id) throws SQLException {
+        String requete = "delete from categorie where id_categorie=?";
+
+        try {
+            this.pst = this.cnx.prepareStatement(requete);
+            this.pst.setInt(1, id);
+            this.pst.executeUpdate();
+            System.out.println("Suppression effectue");
+        } catch (SQLException var4) {
+            throw new RuntimeException(var4);
+        }
+    }
+
+    public void update(Categorie categorie, int id) {
+        if (categorie.getLibelle() != "") {
+            String requete = "update categorie set libelle_categorie=? where id_categorie=?";
+
+            try {
+                this.pst = this.cnx.prepareStatement(requete);
+                this.pst.setString(1, categorie.getLibelle());
+                this.pst.setInt(2, categorie.getId());
+                this.pst.executeUpdate();
+                System.out.println("Modification effectuee");
+            } catch (SQLException var5) {
+                throw new RuntimeException(var5);
+            }
+        }
+
+    }
+
+    public List<Categorie> getAll() {
+        String requete = "select * from categorie";
+        List<Categorie> list = new ArrayList();
+
+        try {
+            this.ste = this.cnx.createStatement();
+            ResultSet rs = this.ste.executeQuery(requete);
+
+            while(rs.next()) {
+                list.add(new Categorie(rs.getInt("id_categorie"), rs.getString(2)));
+            }
+
+            return list;
+        } catch (SQLException var4) {
+            throw new RuntimeException(var4);
+        }
+    }
+
+    public Boolean rechercherCategorie(String lib) {
+
+        String requete = "select * from categorie where libelle_categorie=?";
+        try {
+            this.pst = this.cnx.prepareStatement(requete);
+            this.pst.setString(1, lib);
+            return this.pst.executeQuery().next();
+        } catch (SQLException var4) {
+            throw new RuntimeException(var4);
+        }
+    }
+
+    public List<Integer> afficherIdCategories() {
+        List<Integer> idCategories = new ArrayList<>();
+        String requete = "select id_categorie from categorie";
+        try {
+            // Vérifier si la connexion est ouverte
+            if (cnx != null && !cnx.isClosed()) {
+                PreparedStatement pst = cnx.prepareStatement(requete);
+                ResultSet rs = pst.executeQuery();
+                while (rs.next()) {
+                    idCategories.add(rs.getInt("id_categorie"));
+                }
+            } else {
+                // Gestion de l'erreur de connexion
+                System.out.println("La connexion à la base de données n'est pas ouverte.");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return idCategories;
+    }
+
+    public Categorie getById(int id) {
+        return null;
+    }
+}
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,25 +1,10 @@
-module com.exemple.pidevd {
+module com.example.gestionressourcesmaterielles {
     requires javafx.controls;
     requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-    requires org.kordamp.bootstrapfx.core;
     requires java.sql;
-    requires java.desktop;
-    requires twilio;
-    requires com.sun.jna;
-    requires com.sun.jna.platform;
-    requires org.controlsfx.controls;
-    requires poi;
-    requires poi.ooxml;
-    requires javafx.swing;
-    requires org.jxmapviewer.jxmapviewer2;
-    requires com.gluonhq.maps;
-    requires json.simple;
-    opens com.exemple.pidevd.Model;
-    opens com.exemple.pidevd.Controller to javafx.fxml;
+    opens com.example.gestionressourcesmaterielles.Model;
+    opens com.example.gestionressourcesmaterielles.Controller to javafx.fxml;
 
-    opens com.exemple.pidevd to javafx.fxml;
-    exports com.exemple.pidevd;
-    exports com.exemple.pidevd.Controller;
+    opens com.example.gestionressourcesmaterielles to javafx.fxml;
+    exports com.example.gestionressourcesmaterielles;
 }
