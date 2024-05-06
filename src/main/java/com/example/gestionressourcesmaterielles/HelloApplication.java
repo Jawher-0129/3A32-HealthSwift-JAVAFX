d7e8edd6d2e04b1421d958eaa3ab6347414ea8cf
Jawher-0129 <jawhertalbi12@gmail.com>
2024-05-06 09:58:01 +0100

    Ajout materiel

--- src/main/java/com/example/gestionressourcesmaterielles/HelloApplication.java
+++ src/main/java/com/example/gestionressourcesmaterielles/HelloApplication.java
@@ -15,7 +15,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MaterielInterfaceAdmin.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("CategorieInterfaceFront.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 700);
         stage.setTitle("Hello!");
         stage.setScene(scene);
